605fe9fb237ed9c976c163b990f19c1e5cf534bf
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-03-27 10:31:05 +0530

    update axios base URL for production environment

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE==='development'? 'http://localhost:3000/api': "/api",
+    baseURL: import.meta.env.MODE==='development'? 'http://localhost:3000/api': "https://inair-e6ai.onrender.com/api",
     withCredentials:true
 });
 
     
-export const BACK_END_BASE_URL= import.meta.env.MODE==='development' ? "http://localhost:3000" : "/";
+export const BACK_END_BASE_URL= import.meta.env.MODE==='development' ? "http://localhost:3000" : "https://inair-e6ai.onrender.com/";

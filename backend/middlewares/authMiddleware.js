690c0c8555321883017a1ebc040fc36f95ba52dd
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-03-27 13:40:59 +0530

    update logging in authentication middleware to display cookies

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const protectRoute = async (req, res, next) => {
     
     try {
         const token = req.cookies.token; 
-        console.log(token);
+        console.log(req.cookies);
         if(!token){
             return res.status(401).json({message:"Unauthorized"});
         }
